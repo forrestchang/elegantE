4fdf9e7e5cfdefe99566d7468901ed7bf06e9fb6
Jiayuan <forrestchang7@gmail.com>
2019-04-19 18:50:33 +0800

    test: add example

---
+++ example.py
@@ -0,0 +1,50 @@
+import elegantE as elg
+
+
+def do_something(value):
+    print(value)
+
+
+test_exception_handler = elg.ExceptionHandler
+
+test_exception_handler.regist(
+    Exception,
+    lambda: print('Global Exception')
+)
+
+test_exception_handler.regist(
+    ValueError,
+    do_something,
+    {'value': 'Test value'}
+)
+
+
+class ArgumentError(Exception):
+    pass
+
+
+class NoneArgumentError(ArgumentError):
+    pass
+
+
+def fib(n):
+    if n is None:
+        raise NoneArgumentError
+    if not isinstance(n, int):
+        raise ArgumentError
+
+    if n == 0 or n == 1: return 1
+    return fib(n - 1) + fib(n - 2)
+
+
+@test_exception_handler([
+    (NoneArgumentError, lambda: print('None Argument Error')),
+    (ArgumentError, lambda: print('Argument Error'))
+])
+def main():
+    fib('Hello world')
+
+
+if __name__ == '__main__':
+    main()
+
